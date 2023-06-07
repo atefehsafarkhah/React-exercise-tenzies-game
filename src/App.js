c687bbc059b555b765e9905beaca694d5d16c397
soatsafa <atefeh.safarkhah-shalkouhi@stud.uni-due.de>
2023-06-07 12:37:55 +0200

    Add conditional styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import './style.css'; 
 import Die from './Die';
+import {nanoid} from "nanoid"
 
 
 function App() {
 const [dice,setDice]=React.useState(getRandomNumbersArray())
-
 
 function getRandomNumbersArray(){
   const randomNumbersArray = [];
 
   for (let i = 0; i < 10 ; i++) {
     const randomNumber = Math.ceil(Math.random() * 6);
-    randomNumbersArray.push(randomNumber);
-  }
+    randomNumbersArray.push({value: randomNumber,
+       isHeld:true,
+       id:nanoid()
+  })}
 
   return randomNumbersArray;
 }
+ 
+const diceElements= dice.map(die=>  <Die  key={die.id} value={die.value} isHeld={die.isHeld}  />)
 
-const diceElements= dice.map(die=> <Die value={die}/>)
+
 
 function Roll(){
   setDice(getRandomNumbersArray())
--- src/Die.js
+++ src/Die.js
@@ -46,10 +46,13 @@ export default  function Die(props) {
     }
 ]
 
-  
+ const styles={
+   backgroundColor: props.isHeld? "mediumspringgreen": "white"
+ }
+
    return(
-    <div className="die" >
+    <div className="die" style={styles} >
     <h1>{props.value}</h1>   
     </div>
    ) 
-}
+   }
